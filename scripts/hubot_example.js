b115f90fe9428c4aa6222ed78fc6bdf0e9f465bf
Agnibha <agnibha.bose@gupshup.io>
2017-01-23 10:01:16 +0530

    changed basic functionality.

--- scripts/hubot_example.js
+++ scripts/hubot_example.js
@@ -1,20 +1,12 @@
-var http = require('request');
-var api_key = '{{your_api_key}}';
+var api_key = '7aca6998bd5fe53seabcb868a82e6d168';
 
 
 module.exports = function (jarvis) {
-  jarvis.hear(/[a-zA-Z ]*weather of (\w+)[a-zA-Z ]*[?]*/, function (jar_speech) {
-    var city_name = jar_speech.match[1];
-    console.log("city name ", city_name);
+  jarvis.hear(/weather of (\w+)/i, function (jar_resp) {
+    var city_name = jar_resp.match[1];
     var url = "http://api.openweathermap.org/data/2.5/weather?q=".concat(city_name, "&appid=", api_key, "&units=metric");
-    http({
-      uri: url,
-      method: "GET",
-      timeout: 50000
-    }, function (error, response, body) {
-      if (!error) {
-        jar_speech.send("The avg temparature of ".concat(city_name, " is ", JSON.parse(body).main.temp, " deg c"));
-      }
+    jarvis.http(url).get()(function (err, res, body) {
+      jar_resp.send("The average temparature of " + city_name + " at this moment is " + JSON.parse(body).main.temp + "°C");
     });
   });
 };
