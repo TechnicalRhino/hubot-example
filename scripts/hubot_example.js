978b1e571b1b6ab10fcf6c97052264f037024186
TechnicalRhino <agnibhabose@outlook.com>
2017-01-24 15:00:31 +0530

    Changed Code

--- scripts/hubot_example.js
+++ scripts/hubot_example.js
@@ -1,10 +1,11 @@
-var api_key = '7aca6998bd5fe53seabcb868a82e6d168';
+var api_key = 'ba7b8b6b7d3a9f027da4440ae7ce9f2f';
 
 
 module.exports = function (jarvis) {
   jarvis.hear(/weather of (\w+)/i, function (jar_resp) {
     var city_name = jar_resp.match[1];
     var url = "http://api.openweathermap.org/data/2.5/weather?q=".concat(city_name, "&appid=", api_key, "&units=metric");
+
     jarvis.http(url).get()(function (err, res, body) {
       jar_resp.send("The average temparature of " + city_name + " at this moment is " + JSON.parse(body).main.temp + "°C");
     });
